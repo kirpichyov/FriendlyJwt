f1e0eee7d2e734dc36f767beb19b999cc7deb3a4
Alexandr Kirpichyov <alexandr.kirpichyov@gmail.com>
2022-07-08 12:00:16 +0300

    Implement JwtAuthConfiguration binding

---
+++ src/Kirpichyov.FriendlyJwt.UnitTests/JwtAuthConfigurationTests.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using Bogus;
+using FluentAssertions;
+using Kirpichyov.FriendlyJwt.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+using Xunit;
+
+namespace Kirpichyov.FriendlyJwt.UnitTests
+{
+    [ExcludeFromCodeCoverage]
+    public class JwtAuthConfigurationTests
+    {
+        private readonly Faker _faker;
+
+        public JwtAuthConfigurationTests()
+        {
+            _faker = new Faker();
+        }
+
+        [Fact]
+        public void Bind_PassedConfigurationIsNull_ShouldThrowArgumentNullException()
+        {
+            // Act
+            Action func = () => JwtAuthConfiguration.Bind<TestConfiguration1>(null);
+
+            // Assert
+            func.Should().ThrowExactly<ArgumentNullException>();
+        }
+        
+        [Fact]
+        public void Bind_ConfigurationWithAllPropertiesPassed_ShouldBeEquivalentToExpected()
+        {
+            // Arrange
+            var configuration = new TestConfiguration1()
+            {
+                Issuer = _faker.Internet.Url(),
+                Audience = _faker.Internet.Url(),
+                Secret = _faker.Random.Guid().ToString(),
+                SecurityAlgorithm = SecurityAlgorithms.HmacSha256,
+                RequireHttpsMetadata = true
+            };
+            
+            // Act
+            var result = JwtAuthConfiguration.Bind(configuration);
+
+            // Assert
+            result.Should().BeEquivalentTo(configuration);
+        }
+        
+        [Theory]
+        [InlineData("true", true)]
+        [InlineData("false", false)]
+        [InlineData("True", true)]
+        [InlineData("False", false)]
+        [InlineData(null, false)]
+        [InlineData("other", false)]
+        public void Bind_ConfigurationWithBoolAsStringPropertyPassed_ShouldBeEquivalentToExpected(string value, bool expected)
+        {
+            // Arrange
+            var configuration = new TestConfiguration2()
+            {
+                RequireHttpsMetadata = value
+            };
+            
+            // Act
+            var result = JwtAuthConfiguration.Bind(configuration);
+
+            // Assert
+            result.RequireHttpsMetadata.Should().Be(expected);
+        }
+
+        private record TestConfiguration1
+        {
+            public string Issuer { get; init; }
+            public string Audience { get; init; }
+            public string Secret { get; init; }
+            public string SecurityAlgorithm { get; init; }
+            public bool RequireHttpsMetadata { get; init; }
+        }
+        
+        private record TestConfiguration2
+        {
+            public string RequireHttpsMetadata { get; init; }
+        }
+    }
+}
--- src/Kirpichyov.FriendlyJwt/DependencyInjection/JwtAuthConfiguration.cs
+++ src/Kirpichyov.FriendlyJwt/DependencyInjection/JwtAuthConfiguration.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.IdentityModel.Tokens;
+﻿using System;
+using System.Reflection;
+using Microsoft.IdentityModel.Tokens;
 
 namespace Kirpichyov.FriendlyJwt.DependencyInjection
 {
@@ -12,5 +14,76 @@ namespace Kirpichyov.FriendlyJwt.DependencyInjection
 
         public bool HasIssuer => !string.IsNullOrWhiteSpace(Issuer);
         public bool HasAudience => !string.IsNullOrWhiteSpace(Audience);
+
+        /// <summary>
+        /// Binds the options object to <see cref="JwtAuthConfiguration"/>.
+        /// </summary>
+        /// <param name="optionsObject">Object to obtain configuration from.</param>
+        /// <typeparam name="TOptions">Configuration source object type.</typeparam>
+        /// <returns>Created <see cref="JwtAuthConfiguration"/>.</returns>
+        /// <exception cref="ArgumentNullException">In case if <paramref name="optionsObject"/> is null.</exception>
+        /// <remarks>The only public properties can be used for binding.</remarks>
+        public static JwtAuthConfiguration Bind<TOptions>(TOptions optionsObject)
+            where TOptions : class
+        {
+            string GetCurrentValueAsString(PropertyInfo propertyInfo)
+            {
+                return propertyInfo.GetValue(optionsObject)?.ToString();
+            }
+
+            bool GetCurrentValueAsBool(PropertyInfo propertyInfo)
+            {
+                var rawValue = propertyInfo.GetValue(optionsObject);
+
+                if (rawValue is null)
+                {
+                    return false;
+                }
+
+                if (rawValue is bool boolean)
+                {
+                    return boolean;
+                }
+                
+                if (bool.TryParse(rawValue.ToString(), out var convertedBoolean))
+                {
+                    return convertedBoolean;
+                }
+
+                return false;
+            }
+
+            if (optionsObject is null)
+            {
+                throw new ArgumentNullException(nameof(optionsObject));
+            }
+            
+            var getters = optionsObject.GetType().GetProperties();
+            var jwtConfiguration = new JwtAuthConfiguration();
+
+            foreach (var propertyInfo in getters)
+            {
+                switch (propertyInfo.Name)
+                {
+                    case nameof(Issuer):
+                        jwtConfiguration.Issuer = GetCurrentValueAsString(propertyInfo);
+                        break;
+                    case nameof(Audience):
+                        jwtConfiguration.Audience = GetCurrentValueAsString(propertyInfo);
+                        break;
+                    case nameof(Secret):
+                        jwtConfiguration.Secret = GetCurrentValueAsString(propertyInfo);
+                        break;
+                    case nameof(SecurityAlgorithm):
+                        jwtConfiguration.SecurityAlgorithm = GetCurrentValueAsString(propertyInfo);
+                        break;
+                    case nameof(RequireHttpsMetadata):
+                        jwtConfiguration.RequireHttpsMetadata = GetCurrentValueAsBool(propertyInfo);
+                        break;
+                }
+            }
+
+            return jwtConfiguration;
+        }
     }
 }
